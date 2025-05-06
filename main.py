b675f46b7acd431a66218e0f1ec6bd99e5e50c1b
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-05-06 21:24:05 +0600

    paddle created.

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.color("white")
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
 
 
-
-
-
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 screen.exitonclick()
